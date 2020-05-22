883a93794ab6579b5d55491c305156b8afb64e08
Airad <d.ostapczuk272@wp.pl>
2020-05-23 00:42:11 +0200

    Deleted unused imports

--- todolist/tests.py
+++ todolist/tests.py
@@ -1,10 +1,3 @@
-from datetime import date
-from unittest.mock import ANY
-
-from django.test import TestCase
-
-# Create your tests here.
-from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
